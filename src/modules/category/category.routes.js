6f69c5ea8acddd75093a5315a5b0a655e9bc8a08
jubairahmed22 <jubairahmed060@gmail.com>
2026-05-16 20:04:35 +0600

    added something

--- src/modules/category/category.routes.js
+++ src/modules/category/category.routes.js
@@ -5,7 +5,7 @@ const {
   deleteCategory, 
   updateCategory 
 } = require('./category.controller');
-const { protect, authorize } = require('../../middleware/auth'); fgasdg
+const { protect, authorize } = require('../../middleware/auth');
 const upload = require('../../middleware/upload');
 
 const router = express.Router();
