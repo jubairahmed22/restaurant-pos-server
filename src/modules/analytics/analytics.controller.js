f1cfd6392bc31434856b1cacb59ce8aa2e7251f1
jubairahmed22 <jubairahmed060@gmail.com>
2026-05-21 22:08:17 +0600

    added order perfectly

--- src/modules/analytics/analytics.controller.js
+++ src/modules/analytics/analytics.controller.js
@@ -14,7 +14,7 @@ exports.getDashboardStats = async (req, res, next) => {
 
     const now = new Date();
 
-    const startOfToday = new Date();
+    const startOfToday = new Date();  fadsfs
     startOfToday.setHours(0, 0, 0, 0);
 
     const startOfMonth = new Date(
