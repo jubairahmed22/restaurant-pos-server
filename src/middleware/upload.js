077547e1a148c6df746309deff24b160a7b3bf0c
jubairahmed22 <jubairahmed060@gmail.com>
2026-06-03 08:58:05 +0600

    added reservation function for the api

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -1,7 +1,7 @@
 const multer = require('multer');
 
 const storage = multer.memoryStorage();
-
+sdfsadf
 const fileFilter = (req, file, cb) => {
   if (file.mimetype.startsWith('image/')) {
     cb(null, true);
