e29f91dad06c62435211baab95454fa573e5ce9a
jubairahmed22 <jubairahmed060@gmail.com>
2026-06-05 11:18:37 +0600

    added reservation function for the api

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -9,7 +9,7 @@ const fileFilter = (req, file, cb) => {
     cb(new Error('Only image files are supported!'), false);
   }
 };
-sadfsad
+
 const upload = multer({
   storage: storage,
   limits: { fileSize: 5 * 1024 * 1024 }, // 5MB limit
