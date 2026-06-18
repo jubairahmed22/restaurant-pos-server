5f91e5fd89088a3bac004bddf6114373922b685b
jubairahmed22 <jubairahmed060@gmail.com>
2026-06-19 02:52:04 +0600

    something is added

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 51000;
 connectDB(); 
 
 // Create HTTP Server
-const server = http.createServer(app); asdfsad
+const server = http.createServer(app);
 
 // Initialise real-time engine
 initSocket(server);
