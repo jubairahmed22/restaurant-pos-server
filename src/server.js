98e98d3845d4fadc78e62d276c0132ad58dc315a
jubairahmed22 <jubairahmed060@gmail.com>
2026-06-19 03:02:17 +0600

    something is added

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ connectDB();
 const server = http.createServer(app);
 
 // Initialise real-time engine
-initSocket(server);
+initSocket(server); sadfafsd
 
 // Boot server instance
 server.listen(PORT, () => {
